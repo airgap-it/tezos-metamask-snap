c765b0b6666600efbc3481f9258d40a486612f20
Andreas Gassmann <andreas@andreasgassmann.ch>
2023-06-27 14:56:57 +0200

    chore(snap): use more descriptive names and comments

--- packages/snap/src/tezos/constants.ts
+++ packages/snap/src/tezos/constants.ts
@@ -3,6 +3,22 @@ export const GAS_LIMIT_PLACEHOLDER = '1040000';
 export const STORAGE_LIMIT_PLACEHOLDER = '60000';
 export const FEE_PLACEHOLDER = '0';
 
+export const DEFAULT_GAS_LIMIT = '10300';
+
 export const MINIMAL_FEE = 100;
 export const MINIMAL_FEE_PER_GAS_UNIT = 0.1;
 export const MINIMAL_FEE_PER_BYTE = 1;
+
+export const ORIGINATION_BURN = 257;
+export const ALLOCATION_BURN = 257;
+
+export const REVEAL_GAS_LIMIT = '10000';
+export const REVEAL_STORAGE_LIMIT = '0';
+export const REVEAL_FEE_LIMIT = '1300';
+
+export const GAS_TO_MILLIGAS_MULTIPLIER = 1000;
+
+export const HEX_SIG_LENGTH = 128; // 128 is the length of a hex signature
+export const FEE_SAFETY_DEFAULT = 100; // add 100 for safety, as per reference implementation
+
+export const ALLOCATION_STORAGE_LIMIT = '300';
